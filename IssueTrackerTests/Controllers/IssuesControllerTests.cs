3c112991cf2210c2058a62659d3fdffc912f1929
mbarosendal <mbarosendal@gmail.com>
2026-09-09 10:54:12 +0200

    Add failure test for DeleteAsync and rename success test
    Renamed `DeleteAsyncTest` to `DeleteAsyncTestSuccess` for clarity.
    Added `DeleteAsyncTestFailure` to test the scenario where an issue
    with the specified ID does not exist. Verified that `GetByIdAsync`
    is called once, `SaveChangesAsync` is not called, and the result
    indicates failure with an error type of `NotFound`.
    
    Included `using IssueTracker.Shared;` to support shared types.

--- IssueTrackerTests/Controllers/IssuesControllerTests.cs
+++ IssueTrackerTests/Controllers/IssuesControllerTests.cs
@@ -1,6 +1,7 @@
 ﻿using IssueTracker.Domain;
 using IssueTracker.Infrastructure;
 using IssueTracker.Services;
+using IssueTracker.Shared;
 using Moq;
 
 namespace IssueTrackerTests.Controllers
@@ -9,7 +10,7 @@ namespace IssueTrackerTests.Controllers
     public class IssuesControllerTests
     {
         [TestMethod()]
-        public async Task DeleteAsyncTest()
+        public async Task DeleteAsyncTestSuccess()
         {
             var resultIssue = Issue.Create("Screen flickering", "It's driving me insane.", IssueStatus.Open);
 
@@ -33,5 +34,25 @@ namespace IssueTrackerTests.Controllers
             unitMock.Verify(x => x.SaveChangesAsync(), Times.Once());
             Assert.IsTrue(result.IsSuccess);
         }
+
+        [TestMethod()]
+        public async Task DeleteAsyncTestFailure()
+        {
+            Mock<IIssueStore> storeMock = new();
+            storeMock
+                .Setup(x => x.GetByIdAsync(123))
+                .ReturnsAsync((Issue?)null);
+            Mock<IUnitOfWork> unitMock = new();
+
+            IssueService issueService = new(storeMock.Object, unitMock.Object);
+
+            var result = await issueService.DeleteIssueAsync(123);
+
+            storeMock.Verify(x => x.GetByIdAsync(123), Times.Once());
+            unitMock.Verify(x => x.SaveChangesAsync(), Times.Never());
+            Assert.IsTrue(result.IsFailure);
+            Assert.AreEqual(ErrorType.NotFound, result.Error.Type);
+        }
+
     }
 }
