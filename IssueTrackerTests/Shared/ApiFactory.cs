c79b0c3367aab2bae7fad9b9e556e5438041dc36
mbarosendal <mbarosendal@gmail.com>
2026-09-21 12:05:29 +0200

    Fix API integration test database configuration

--- IssueTrackerTests/Shared/ApiFactory.cs
+++ IssueTrackerTests/Shared/ApiFactory.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
@@ -17,9 +18,10 @@ internal sealed class ApiFactory(string connectionString) : WebApplicationFactor
 
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
-        builder.ConfigureServices(services =>
+        builder.ConfigureTestServices(services =>
         {
             services.RemoveAll<AppDbContext>();
+            services.RemoveAll<DbContextOptions<AppDbContext>>();
 
             services.AddDbContext<AppDbContext>(options =>
                 options.UseSqlServer(_connectionString));
